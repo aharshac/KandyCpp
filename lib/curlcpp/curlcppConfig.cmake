8aaa78bf5f50a8480e4c47daa0b4b7418d5e5a66
Harsha <Harsha>
2017-03-20 21:08:35 +0530

    Added curlcpp files

---
+++ lib/curlcpp/curlcppConfig.cmake
@@ -0,0 +1,5 @@
+# curlcppConfig.cmake - Configuration file for external projects.
+
+set(CURLCPP_INCLUDE_DIRS "/home/harsha/cpp/lib/curlcpp/include")
+set(CURLCPP_LIBRARY_DIRS "/home/harsha/cpp/lib/curlcpp/src")
+set(CURLCPP_LIBRARIES "$<TARGET_FILE:>")
