9f3c24c2c79dbb25d7f34e0d4b8a9ad9b3979548
Harsha <Harsha>
2017-03-20 19:38:41 +0530

    Added cmake file

---
+++ kandyConfig.cmake
@@ -0,0 +1,5 @@
+# kandyConfig.cmake - Configuration file for external projects.
+
+set(KANDY_INCLUDE_DIRS "/home/harsha/cpp/include")
+set(KANDY_LIBRARY_DIRS "/home/harsha/cpp/src")
+set(KANDY_LIBRARIES "$<TARGET_FILE:>")
