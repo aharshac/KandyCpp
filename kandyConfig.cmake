4a77ff7e62905a05b05ee77b01dd1dd617bdf6ac
Harsha <Harsha>
2017-03-21 09:27:14 +0530

    Added cmake loc var

--- kandyConfig.cmake
+++
@@ -1,5 +0,0 @@
-# kandyConfig.cmake - Configuration file for external projects.
-
-set(KANDY_INCLUDE_DIRS "/home/harsha/cpp/include")
-set(KANDY_LIBRARY_DIRS "/home/harsha/cpp/src")
-set(KANDY_LIBRARIES "$<TARGET_FILE:>")
